ea4c9e1bd06eb12b6615734f0aa2c76e3299c988
daegeun.ha <daegeunhaha@gmail.com>
2023-01-31 02:16:53 +0900

    refactoring to add typing hints. before resolve TypeVar inheritence

--- 1.Money/bank.py
+++ 1.Money/bank.py
@@ -1,12 +1,17 @@
 from expression import Expression
 from typing import TypeVar
+from money import Money
 
-T = TypeVar('T', bound='Bank')
+T = TypeVar('T', 'Bank')
+
+# inhertence class에 대해 TypeVar를 어떻게 처리해야 하나 잘 모르겠다. bound to Bank로 해야 하나? 그냥 해두면 알아서 되나?
+# 그럼 내가 TypeVar 정할 때 미래에 해당 클래스를 상속할 지도 모르는 것을 고려해서 해야하나? 흠..
+# https://stackoverflow.com/questions/47896283/how-to-have-inherited-type-hints-in-python
 
 class Bank():
 
     def __init__(self: T) -> None:
         pass
     
-    def reduce(self: T, expression: Expression) -> None:
+    def reduce(self: T, expression: Expression) -> Money:
         pass
